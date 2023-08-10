01eae150613ae28c9570a3b52130c679a796bdc1
suttoMatrix <gabriel.sutto@matrixenergia.com>
2023-08-10 17:21:56 -0300

    creating shell script to create dns on cloudflare

---
+++ Baseline files/swa/utils/update_dns.sh
@@ -0,0 +1,25 @@
+config_file="../.ci/${ENVIRONMENT}-values.yaml"
+config=$(yaml2json < "$config_file")
+
+custom_host=$(echo "$config" | jq -r '.customHost')
+enable=$(echo "$custom_host" | jq -r '.enabled')
+
+if [ "$enable" = "true" ]; then
+  url=$(echo "$custom_host" | jq -r '.url')
+else
+  url="$DEFAULT_URL"
+fi
+
+echo "URL: $url"
+
+response=$(curl -s https://dev-app.matrixenergia.com/auto-dns/check_dns/$url )
+
+if [[ "$response" == *"\"exist\": true"* ]]; then
+    echo "A primeira API retornou 'exist': true"
+else
+    echo "A primeira API retornou 'exist': false"
+    
+    # Chamar a segunda API
+    second_response=$(curl --location --request POST https://dev-app.matrixenergia.com/auto-dns/create_dns/$url )
+    echo "Resposta da segunda API: $second_response"
+fi
