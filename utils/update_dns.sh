cbc221df5932e9b92ea3de7c474496284eced04c
suttoMatrix <gabriel.sutto@matrixenergia.com>
2023-08-11 10:43:55 -0300

    chore: changing how get vars from yaml

--- utils/update_dns.sh
+++ utils/update_dns.sh
@@ -1,13 +1,11 @@
 environment=$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]')
 
 config_file=".ci/${environment}-values.yaml"
-config=$(yq eval '.' "$config_file")
+enable=$(yq eval '.customHost.enabled' "$config_file")
 
-custom_host=$(echo "$config" | jq -r '.customHost')
-enable=$(echo "$custom_host" | jq -r '.enabled')
 
 if [ "$enable" = "true" ]; then
-  url=$(echo "$custom_host" | jq -r '.url')
+  url=$(yq eval '.customHost.url' "$config_file")
 else
   url="$DEFAULT_URL"
 fi
@@ -17,11 +15,8 @@ echo "URL: $url"
 response=$(curl -s https://dev-app.matrixenergia.com/auto-dns/check_dns/$url )
 
 if [[ "$response" == *"\"exist\": true"* ]]; then
-    echo "A primeira API retornou 'exist': true"
+    echo "DNS already exists"
 else
-    echo "A primeira API retornou 'exist': false"
-    
-    # Chamar a segunda API
     second_response=$(curl --location --request POST https://dev-app.matrixenergia.com/auto-dns/create_dns/$url )
-    echo "Resposta da segunda API: $second_response"
+    echo "creating a new dns register with: $second_response"
 fi
